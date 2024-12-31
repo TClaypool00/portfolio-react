dde723a7fee850f3a7ede30cee14000c297bf522
TClaypool00 <claypooltyler00@gmail.com>
2024-12-30 23:39:54 -0600

    Added helper function to clear error paragraph

--- portfolio/src/helpers/pageHelper.js
+++ portfolio/src/helpers/pageHelper.js
@@ -3,3 +3,7 @@ import values from '../values.json';
 export function setTitle(index) {
     document.title = `${values.pages[index].text} - ${values.title}`;
 }
+
+export function clearErrorMessage(ref) {
+    ref.current.innerHTML = '';
+}
