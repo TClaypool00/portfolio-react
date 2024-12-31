24166e7ea43117b196de8c1bc120323c14bf63f4
TClaypool00 <claypooltyler00@gmail.com>
2024-12-30 23:38:33 -0600

    Added paragraph to display error messages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,15 +4,20 @@ import values from './values.json';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import ToDo from './pages/ToDo';
+import { useRef } from 'react';
 
 function App() {
+  const errorMessageRef = useRef(null);
+
   return (
     <Router>
       <NavBar />
       <div className='main-content'>
+        <p ref={errorMessageRef} id='errorMessage'></p>
+
         <Routes>
           <Route path={values.pages[0].url} element={<Home />} />
-          <Route path={values.pages[1].url} element={<ToDo />} />
+          <Route path={values.pages[1].url} element={<ToDo errorMessageRef={errorMessageRef} />} />
         </Routes>
       </div>
     </Router>
